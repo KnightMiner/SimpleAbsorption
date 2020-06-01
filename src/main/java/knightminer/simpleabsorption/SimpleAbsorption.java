09af7ef47a8ab3e270f1e7a07126981a4ce03a3c
KnightMiner <knightminer4@gmail.com>
2020-05-31 23:39:29 -0500

    Add absorption tooltip for the enchant and gold armor

--- src/main/java/knightminer/simpleabsorption/SimpleAbsorption.java
+++ src/main/java/knightminer/simpleabsorption/SimpleAbsorption.java
@@ -2,10 +2,12 @@ package knightminer.simpleabsorption;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.Enchantment.Rarity;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.common.MinecraftForge;
@@ -41,14 +43,22 @@ public class SimpleAbsorption {
 	}
 
 	private static void itemTooltip(ItemTooltipEvent event) {
+		// tooltips only matter for gold armor or the enchant
 		int goldBoost = Config.GOLD_ABSORPTION.get();
-		if (goldBoost <= 0) {
+		if (goldBoost == 0 && Config.MAX_ENCHANT.get() == 0) {
 			return;
 		}
-		// add tooltip to golden armor
-		Item item = event.getItemStack().getItem();
+		// enchantment level
+		ItemStack stack = event.getItemStack();
+		int absorption = EnchantmentHelper.getEnchantmentLevel(ABSORPTION, stack);
+		// gold boost
+		Item item = stack.getItem();
 		if (item instanceof ArmorItem && ((ArmorItem)item).getArmorMaterial() == ArmorMaterial.GOLD) {
-			event.getToolTip().add(new TranslationTextComponent("tooltip.simple_absorption.golden_armor", goldBoost).applyTextStyle(TextFormatting.BLUE));
+			absorption += goldBoost;
+		}
+		// add tooltip if relevant
+		if (absorption > 0) {
+			event.getToolTip().add(new TranslationTextComponent("tooltip.simple_absorption.absorption", absorption).applyTextStyle(TextFormatting.BLUE));
 		}
 	}
 }
