c920eca77da90b294a4c7dc7f6f3641fcc73de06
KnightMiner <knightminer4@gmail.com>
2020-05-31 23:15:43 -0500

    Add absorption tooltip to armor
    TODO: make this an attribute?

--- src/main/java/knightminer/simpleabsorption/SimpleAbsorption.java
+++ src/main/java/knightminer/simpleabsorption/SimpleAbsorption.java
@@ -3,8 +3,14 @@ package knightminer.simpleabsorption;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.Enchantment.Rarity;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Item;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -24,6 +30,7 @@ public class SimpleAbsorption {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, Config.SPEC);
 		FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(Enchantment.class, SimpleAbsorption::registerEnchants);
 		MinecraftForge.EVENT_BUS.addListener(AbsorptionHandler::playerTick);
+		MinecraftForge.EVENT_BUS.addListener(SimpleAbsorption::itemTooltip);
 	}
 
 	/** Registers mod applicable enchants */
@@ -31,5 +38,17 @@ public class SimpleAbsorption {
 		Enchantment absorption = new AbsorptionEnchantment(Rarity.RARE, EquipmentSlotType.HEAD, EquipmentSlotType.CHEST, EquipmentSlotType.LEGS, EquipmentSlotType.FEET);
 		absorption.setRegistryName(ENCHANT_ID);
 		event.getRegistry().register(absorption);
+	}
+
+	private static void itemTooltip(ItemTooltipEvent event) {
+		int goldBoost = Config.GOLD_ABSORPTION.get();
+		if (goldBoost <= 0) {
+			return;
+		}
+		// add tooltip to golden armor
+		Item item = event.getItemStack().getItem();
+		if (item instanceof ArmorItem && ((ArmorItem)item).getArmorMaterial() == ArmorMaterial.GOLD) {
+			event.getToolTip().add(new TranslationTextComponent("tooltip.simple_absorption.golden_armor", goldBoost).applyTextStyle(TextFormatting.BLUE));
+		}
 	}
 }
