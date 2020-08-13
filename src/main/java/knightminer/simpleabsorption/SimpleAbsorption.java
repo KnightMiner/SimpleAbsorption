8d61e02ecd97cec00f69aa42032a64b805a0d2e7
KnightMiner <knightminer4@gmail.com>
2020-08-12 20:08:11 -0500

    Move tooltip event to client class, prevent unsafe server calls

--- src/main/java/knightminer/simpleabsorption/SimpleAbsorption.java
+++ src/main/java/knightminer/simpleabsorption/SimpleAbsorption.java
@@ -2,17 +2,9 @@ package knightminer.simpleabsorption;
 
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.Enchantment.Rarity;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.inventory.EquipmentSlotType;
-import net.minecraft.item.ArmorItem;
-import net.minecraft.item.ArmorMaterial;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent.Register;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -36,7 +28,6 @@ public class SimpleAbsorption {
 		modBus.addGenericListener(Enchantment.class, SimpleAbsorption::registerEnchants);
 		modBus.addListener(Config::configChanged);
 		MinecraftForge.EVENT_BUS.addListener(AbsorptionHandler::playerTick);
-		MinecraftForge.EVENT_BUS.addListener(SimpleAbsorption::itemTooltip);
 	}
 
 	/** Registers mod applicable enchants */
@@ -44,26 +35,5 @@ public class SimpleAbsorption {
 		Enchantment absorption = new AbsorptionEnchantment(Rarity.RARE, EquipmentSlotType.HEAD, EquipmentSlotType.CHEST, EquipmentSlotType.LEGS, EquipmentSlotType.FEET);
 		absorption.setRegistryName(ENCHANT_ID);
 		event.getRegistry().register(absorption);
-	}
-
-	/** Adds the absorption tooltip to armor */
-	private static void itemTooltip(ItemTooltipEvent event) {
-		// tooltips only matter for gold armor or the enchant
-		int goldBoost = Config.GOLD_ABSORPTION.get();
-		if (goldBoost == 0 && Config.MAX_ENCHANT.get() == 0) {
-			return;
-		}
-		// enchantment level
-		ItemStack stack = event.getItemStack();
-		int absorption = EnchantmentHelper.getEnchantmentLevel(ABSORPTION, stack);
-		// gold boost
-		Item item = stack.getItem();
-		if (item instanceof ArmorItem && ((ArmorItem)item).getArmorMaterial() == ArmorMaterial.GOLD) {
-			absorption += goldBoost;
-		}
-		// add tooltip if relevant
-		if (absorption > 0) {
-			event.getToolTip().add(new TranslationTextComponent("tooltip.simple_absorption.absorption", absorption).mergeStyle(TextFormatting.BLUE));
-		}
 	}
 }
